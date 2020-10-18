8f8e4a82df6a21ef3ae93a854664810dacd8f042
Wisemuji <wisemuji@gmail.com>
2020-10-18 20:39:02 +0900

    [#3] Show camera activity with image capture indent

--- app/src/main/java/com/coveong/bankacountscanner/MainActivity.kt
+++ app/src/main/java/com/coveong/bankacountscanner/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.coveong.bankacountscanner
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERA_PIC_REQUEST) {
+            val image = data?.extras!!["data"] as Bitmap?
+            preview_imageView.setImageBitmap(image)
+        }
+    }
+
+    companion object {
+        const val CAMERA_PIC_REQUEST = 100
     }
 }
