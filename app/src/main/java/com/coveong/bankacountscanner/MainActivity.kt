7a8690e70f22e703c9615e636682f52e9e691334
Wisemuji <wisemuji@gmail.com>
2020-11-01 02:51:28 +0900

    [#10] Show Camera activity when preview_imageView is empty

--- app/src/main/java/com/coveong/bankacountscanner/MainActivity.kt
+++ app/src/main/java/com/coveong/bankacountscanner/MainActivity.kt
@@ -1,30 +1,21 @@
 package com.coveong.bankacountscanner
 
+import com.coveong.bankacountscanner.ui.camera.CameraActivity
 import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(cameraIntent, CAMERA_PIC_REQUEST)
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CAMERA_PIC_REQUEST) {
-            val image = data?.extras!!["data"] as Bitmap?
-            preview_imageView.setImageBitmap(image)
+        if (preview_imageView.background == null) {
+            // TODO: startActivityForResult로 변경해서 사진을 받아와야 함
+            startActivity(Intent(this, CameraActivity::class.java))
         }
-    }
-
-    companion object {
-        const val CAMERA_PIC_REQUEST = 100
     }
 }
