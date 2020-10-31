a46c1fefc98f13d37f6ee20cbed896fe3aecf3f1
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2020-11-01 00:32:07 +0900

    [#8] API키 설정 테스트, BuildConfig 삭제

---
+++ app/src/main/java/com/coveong/bankacountscanner/MainApplication.kt
@@ -0,0 +1,10 @@
+package com.coveong.bankacountscanner
+
+import android.app.Application
+import android.util.Log
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
