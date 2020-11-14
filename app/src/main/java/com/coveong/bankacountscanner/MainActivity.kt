f17a56fbabd8e74b236027fb04efb693c918bf30
Wisemuji <wisemuji@gmail.com>
2020-11-15 02:04:31 +0900

    [#29] Initialize progress dialog

--- app/src/main/java/com/coveong/bankacountscanner/MainActivity.kt
+++ app/src/main/java/com/coveong/bankacountscanner/MainActivity.kt
@@ -5,17 +5,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import android.Manifest
+import android.app.Dialog
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.graphics.drawable.toBitmap
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -27,6 +23,7 @@ import com.coveong.bankacountscanner.ui.camera.CameraRepository
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainViewModel: MainViewModel
+    private lateinit var progressDialog: Dialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         initializeMainViewModel()
         settingContentView()
+        initializeProgressDialog()
     }
 
     private fun requestPreviewImage() {
@@ -70,6 +68,13 @@ class MainActivity : AppCompatActivity() {
         ).apply {
             viewModel = mainViewModel
             lifecycleOwner = this@MainActivity
+        }
+    }
+
+    private fun initializeProgressDialog() {
+        progressDialog = Dialog(this).apply {
+            setContentView(R.layout.dialog_progress)
+            setCancelable(false)
         }
     }
 
