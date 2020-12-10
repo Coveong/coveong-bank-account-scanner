607813031030bdb9f0da9b0f8ccbf93e94cc936e
Shin Sunyoung <s2017i30@e-mirim.hs.kr>
2020-12-10 23:20:42 +0900

    [#36] 수협 제거

--- app/src/main/java/com/coveong/bankacountscanner/util/CoveongAccountParser.kt
+++ app/src/main/java/com/coveong/bankacountscanner/util/CoveongAccountParser.kt
@@ -47,7 +47,7 @@ object CoveongAccountParser {
         val allBanks = arrayOf(
             "국민", "카카오", "기업", "농협", "신한", "산업", "우리", "씨티",
             "하나", "제일", "경남", "광주", "대구", "도이치", "부산", "저축",
-            "새마을", "수협", "수협", "신협", "우체국", "전북", "제주", "케이"
+            "새마을", "수협", "신협", "우체국", "전북", "제주", "케이"
         )
         for (bank in allBanks) {
             if (account.contains(bank)) {
